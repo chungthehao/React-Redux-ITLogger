c50c61b38eedc86b8d48b11fb9854c17c04fffe8
Chung Thế Hào <chungthehao@gmail.com>
2019-10-15 22:04:52 +0700

    Materialize Setup (materialize-css, material design icons& clean up)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
+
+import 'materialize-css/dist/css/materialize.min.css'
+import M from 'materialize-css/dist/js/materialize.min.js'
 import './App.css';
 
-function App() {
+const App = () => {
+  // Tự động init materialize js khi mới chạy app
+  useEffect(() => {
+    M.AutoInit() // and now we should be able to use modals and all that stuff (có dùng js)
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      My App
     </div>
   );
 }
